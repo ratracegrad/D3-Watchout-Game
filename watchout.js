fb219b37cfe1c3f72e5516d1a1075067c3057772
Jennifer Bland <ratracegrad@gmail.com>
2015-04-03 19:54:09 -0400

    Step #2 - Make it so that the enemies move to a new random location every second.

--- watchout.js
+++ watchout.js
@@ -8,6 +8,14 @@ function drawAsteroids() {
       .attr("cy", Math.floor(Math.random() * 400))
       .attr("style", "fill:steelblue;")
       .attr("r", 20);
+
+      setInterval(function(){
+        circle.transition()
+        .duration(750)
+        .attr("cx", Math.floor(Math.random() * 900))
+        .attr("cy", Math.floor(Math.random() * 400));
+      }, 1000);
+
 };
 
 for (var i = 0; i < 10; i++) {
