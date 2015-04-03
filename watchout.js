dbb45109899f431680ccdccc53e866c3f373cdfc
Jennifer Bland <ratracegrad@gmail.com>
2015-04-03 19:45:06 -0400

    Step #1 Draw the enemies in an svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,15 @@
-// start slingin' some d3 here.
+var svgContainer = d3.select("body").append("svg")
+     .attr("width", 960)
+     .attr("height", 500);
 
+function drawAsteroids() {
+    var circle = svgContainer.append("circle")
+      .attr("cx", Math.floor(Math.random() * 400))
+      .attr("cy", Math.floor(Math.random() * 300))
+      .attr("style", "fill:steelblue;")
+      .attr("r", 20);
+};
+
+for (var i = 0; i < 10; i++) {
+  drawAsteroids();
+}
