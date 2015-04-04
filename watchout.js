a43e4c4615c9ce99a06261f76888e12b16dc130d
Jennifer Bland <ratracegrad@gmail.com>
2015-04-03 20:08:20 -0400

    Step #3 - Make a differently colored dot to represent the player. Make it draggable.

--- watchout.js
+++ watchout.js
@@ -2,6 +2,8 @@ var svgContainer = d3.select("body").append("svg")
      .attr("width", 960)
      .attr("height", 500);
 
+
+
 function drawAsteroids() {
     var circle = svgContainer.append("circle")
       .attr("cx", Math.floor(Math.random() * 900))
@@ -15,9 +17,31 @@ function drawAsteroids() {
         .attr("cx", Math.floor(Math.random() * 900))
         .attr("cy", Math.floor(Math.random() * 400));
       }, 1000);
-
 };
 
+function drawPlayer() {
+  var drag = d3.behavior.drag()  
+     .on('dragstart', function() { circle.style('fill', 'red'); })
+     .on('drag', function() { circle.attr('cx', d3.event.x)
+                                    .attr('cy', d3.event.y); })
+     .on('dragend', function() { circle.style('fill', 'violet'); });
+
+  var circle = svgContainer.append("circle")
+      .attr("cx", Math.floor(Math.random() * 900))
+      .attr("cy", Math.floor(Math.random() * 400))
+      .attr("style", "fill:violet;")
+      .attr("r", 20)
+      .call(drag);
+}
+
+
+//-------------------------------------------------
+//  Actual start playing of game below
+//-------------------------------------------------
 for (var i = 0; i < 10; i++) {
   drawAsteroids();
-}
+};
+
+drawPlayer();
+
+
